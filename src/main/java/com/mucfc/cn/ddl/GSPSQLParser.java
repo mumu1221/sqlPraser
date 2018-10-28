a57b13d689910f9f096b9d70b60f81cf42ecaa95
sunhao <2855971058@qq.com>
2018-10-29 00:20:31 +0800

    sdlsakd

--- src/main/java/com/mucfc/cn/ddl/GSPSQLParser.java
+++ src/main/java/com/mucfc/cn/ddl/GSPSQLParser.java
@@ -26,24 +26,23 @@ public class GSPSQLParser {
         String alterDDLOne = "ALTER TABLE tbl_name change a b int ;";
         String alterDDLModify = "ALTER TABLE tbl_name modify a  int ,change b c varchar(100);";
 
-        TGSqlParser mysqlSqlParser = new TGSqlParser(EDbVendor.dbvmysql);
-        mysqlSqlParser.sqltext = createDDL;
-        int rs = mysqlSqlParser.checkSyntax();
+        TGSqlParser verticaSqlParser = new TGSqlParser(EDbVendor.dbvvertica);
+        verticaSqlParser.sqltext = verticaCreateTableDDL;
+        int rs = verticaSqlParser.checkSyntax();
         if(rs == 0){
             System.out.println("语法正确！");
         }else{
-            System.out.println("语法错误："+mysqlSqlParser.getErrormessage());
+            System.out.println("语法错误："+verticaSqlParser.getErrormessage());
         }
 
         System.out.println();
 
-        TCreateTableSqlStatement createTable  = (TCreateTableSqlStatement)mysqlSqlParser.sqlstatements.get(0);
+        TCreateTableSqlStatement createTable  = (TCreateTableSqlStatement)verticaSqlParser.sqlstatements.get(0);
 
         System.out.println(createTable.getTableComment());
-        System.out.println(createTable.getColumnList().getColumn(0));
         System.out.println(createTable.getTableName());
-        System.out.println(createTable.getMySQLTableOptionList());
 
         System.out.println(createTable.getTableConstraints());
+        System.out.println(createTable.getColumnList());
     }
 }
